4f1a470687e45f7fc450ff96fc75e22b9647045d
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-04-11 14:45:18 +0200

    Improve footer text formatting

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -17,7 +17,7 @@ export function Footer() {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="w-4 h-4 inline-block text-red-500 animate-pulse-soft"
+          className="w-4 h-4 inline-block text-red-500 animate-heartbeat"
         >
           <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
         </svg>
@@ -58,7 +58,8 @@ export function Footer() {
           aria-label="Vercel (opens in a new tab)"
         >
           Vercel
-        </a>. Set in{' '}
+        </a>. {' '}
+        Set in the{' '}
         <a 
           href="https://rsms.me/inter/" 
           className="font-bold hover:text-indigo-500 dark:hover:text-indigo-400 transition-colors duration-200" 
@@ -67,7 +68,9 @@ export function Footer() {
           aria-label="Inter (opens in a new tab)"
         >
           Inter
-        </a>—because it simply looks right.
+        </a>{' '}
+        typeface. {' '}
+        It simply looks right.
       </p>
     </footer>
   );
